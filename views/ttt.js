9aacb790f5d2cf22b06fa288d23183b86dd938f0
miss-tree <45055914+miss-tree@users.noreply.github.com>
2020-03-13 00:17:16 +0800

    0313修改

--- views/ttt.js
+++
@@ -1,7 +0,0 @@
-&lt;div&gt; {{name|componentFilter}} &lt;/div&gt;
-&lt;div :data="ok|componentFilter"&gt; hello world &lt;/div&gt;,
-
-也可以是多个
-		{{message|filterA|filterB}}
-当变量		message:2019,filterA:"年",filterB="hello",
-=&gt;渲染出来  		2019年hello
